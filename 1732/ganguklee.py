5a6f9f91b2091c717ab50d48cf1bb893b16408d8
gangUK <126441816+ganguklee@users.noreply.github.com>
2024-06-11 00:35:52 +0900

    Create ganguklee.py

---
+++ 1732/ganguklee.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        max_alt, alt = 0, 0
+        for g in gain:
+            alt += g
+            max_alt = max(max_alt, alt)
+        return max_alt
