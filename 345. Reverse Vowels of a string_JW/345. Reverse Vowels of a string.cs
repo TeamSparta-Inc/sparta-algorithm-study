f8028c14b75c61656d23166643b06457b8eb2f3f
SpartaJungWoo <114643828+SpartaJungWoo@users.noreply.github.com>
2024-06-18 13:26:49 +0900

    2024/06/18

---
+++ 345. Reverse Vowels of a string_JW/345. Reverse Vowels of a string.cs
@@ -0,0 +1,51 @@
+public class Solution
+{
+    public string ReverseVowels(string s)
+    {
+        List<char> list = new List<char>();
+        List<int> listCount = new List<int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            switch (s[i])
+            {
+                case 'a':
+                case 'A':
+                case 'e':
+                case 'E':
+                case 'i':
+                case 'I':
+                case 'o':
+                case 'O':
+                case 'u':
+                case 'U':
+                    list.Insert(0, s[i]);
+                    listCount.Add(i);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (list.Count <= 1) return s;
+
+        StringBuilder result = new StringBuilder();
+
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (listCount.Count > 0 && i == listCount[0])
+            {
+                Console.WriteLine(list[0]);
+                result.Append(list[0]);
+                list.RemoveAt(0);
+                listCount.RemoveAt(0);
+                continue;
+            }
+
+            result.Append(s[i]);
+        }
+
+        return result.ToString();
+    }
+}
