71c52428f8d11fee6072233612db44b6586ded15
5startaek <st.oh@teamsparta.co>
2024-06-18 12:52:44 +0900

    refactor

--- 1732-Find-the-Highest-Altitude/ost.ts
+++ 1732-Find-the-Highest-Altitude/ost.ts
@@ -2,8 +2,8 @@ function largestAltitude(gain: number[]): number {
     let initialAltitudePoint = 0;
     let highestAltitudePoint = 0;
     gain.forEach(altitudePoint => {
-            initialAltitudePoint += altitudePoint;
-            if(initialAltitudePoint > highestAltitudePoint) highestAltitudePoint = initialAltitudePoint
+        initialAltitudePoint += altitudePoint;
+        if(initialAltitudePoint > highestAltitudePoint) highestAltitudePoint = initialAltitudePoint
         }
     )
     return highestAltitudePoint
