8eb9142f68152bf3884f5fc109b4cf61f0f613dd
sangyeopchae <sy.chae@teamsparta.co>
2024-06-18 12:42:28 +0900

    feat: 700번 풀이

---
+++ 700-Search-In-A-Binary-Search-Tree/sangyeopchae.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        return self.searchBST(root.left, val)
