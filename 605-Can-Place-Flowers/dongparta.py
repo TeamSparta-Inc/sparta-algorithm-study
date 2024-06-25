ce71ba8aed228398a62e607b233984af85fa1a67
dongparta <donghee.lee@teamsparta.co>
2024-06-25 13:15:02 +0900

    donghee

---
+++ 605-Can-Place-Flowers/dongparta.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        flowerbed = [0] + flowerbed + [0]
+        count = 0
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i-1:i+2] == [0, 0, 0]:
+                flowerbed[i] = 1
+                count += 1
+            if count >= n:
+                return True
+        return False
