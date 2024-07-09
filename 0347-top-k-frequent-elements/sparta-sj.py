3d0dd96f578a8ae02fa8645e4d1c0b34eaee82d6
sparta-sj <sj.shin@teamsparta.co>
2024-07-09 12:40:22 +0900

    Time: 110 ms (7.92%), Space: 18.7 MB (100%) - LeetHub

---
+++ 0347-top-k-frequent-elements/sparta-sj.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        data = defaultdict(int)
+        heap = list()
+        result = list()
+        
+        for x in nums:
+            data[x] += 1
+            
+        for key, value in data.items():
+            heapq.heappush(heap, (-value, key))
+            
+        for i in range(k):
+            tmp = heapq.heappop(heap)
+            result.append(tmp[1])
+            
+        return result
+            
