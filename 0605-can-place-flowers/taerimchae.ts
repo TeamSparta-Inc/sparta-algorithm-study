f89b608320b48ee57854e0da13bab71415c9dfa0
taerimchae <tr.chae@teamsparta.co>
2024-07-30 12:56:23 +0900

    Time: 53 ms (93.22%), Space: 52.4 MB (31.59%) - LeetHub

---
+++ 0605-can-place-flowers/taerimchae.ts
@@ -0,0 +1,14 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+        if (i === 0 || flowerbed[i - 1] === 0) {
+            if (i === flowerbed.length - 1 || flowerbed[i + 1] === 0) {
+            flowerbed[i] = 1;
+            count++;
+            }
+        }
+        }
+    }
+    return count >= n;
+}
