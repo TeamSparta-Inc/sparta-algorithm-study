83065de38ebe8f0d14bd983a75f182306706bb20
junhwanseo <junhwan.seo@teamsparta.co>
2024-08-06 12:51:32 +0900

    Time: 53 ms (71.39%), Space: 50 MB (55.28%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/junhwanseo.ts
@@ -0,0 +1,14 @@
+function tribonacci(n: number): number {
+   if (n === 0) return 0;
+   if (n === 1 || n === 2) return 1;
+
+   const series = [0, 1, 1];
+
+   let i = 3;
+
+   do {
+      series[i] = series[i - 3] + series[i - 2] + series[i++ - 1];
+   } while (i <= n);
+   
+   return series[n];
+};
