b07c78b9e92b8a62a9fd48c1195b7ca7f5289654
taerimchae <tr.chae@teamsparta.co>
2024-08-20 12:55:24 +0900

    Time: 92 ms (23.52%), Space: 55.6 MB (24.35%) - LeetHub

---
+++ 0283-move-zeroes/taerimchae.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    let lastNonZeroFoundAt = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+        nums[lastNonZeroFoundAt++] = nums[i];
+        }
+    }
+    for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+    console.log(nums);
+};
