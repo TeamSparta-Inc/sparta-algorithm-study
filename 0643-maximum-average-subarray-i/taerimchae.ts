428f639ddb8208aae829e097e8da08cabec48c98
taerimchae <tr.chae@teamsparta.co>
2024-09-03 12:47:07 +0900

    Time: 92 ms (33.37%), Space: 63.9 MB (77.19%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/taerimchae.ts
@@ -0,0 +1,12 @@
+function findMaxAverage(nums: number[], k: number): number {
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    let max = sum;
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k];
+        max = Math.max(max, sum);
+    }
+    return max / k;
+}
