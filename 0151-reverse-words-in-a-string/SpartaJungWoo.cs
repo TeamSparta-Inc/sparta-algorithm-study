852f6eb5f0e9632b8eb7c0289df6cae5b81d34a3
SpartaJungWoo <jw.lee@teamsparta.co>
2024-09-10 12:52:36 +0900

    Time: 53 ms (83.98%), Space: 40.5 MB (91%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/SpartaJungWoo.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+}
