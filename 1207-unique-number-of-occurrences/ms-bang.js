c0bde3d9e62e79bbe0d4af88aefe6cda56a1a770
ms-bang <ms.bang@teamsparta.co>
2024-09-24 12:54:25 +0900

    Time: 61 ms (27.01%), Space: 50.5 MB (46.15%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/ms-bang.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const freq = new Map();
+    for (const x of arr) {
+        freq.set(x, (freq.get(x) || 0) + 1);
+    }
+
+    const s = new Set(freq.values());
+    return freq.size === s.size;
+};
