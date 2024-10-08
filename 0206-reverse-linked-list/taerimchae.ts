edf8372d1ca2c0744ebb20ee73e759945b1d2961
taerimchae <tr.chae@teamsparta.co>
2024-10-08 12:53:07 +0900

    Time: 71 ms (19.57%), Space: 52.7 MB (43.23%) - LeetHub

---
+++ 0206-reverse-linked-list/taerimchae.ts
@@ -0,0 +1,26 @@
+// class ListNode {
+//     val: number
+//     next: ListNode | null
+//       constructor(val?: number, next?: ListNode | null) {
+//           this.val = (val===undefined ? 0 : val)
+//           this.next = (next===undefined ? null : next)
+//       }
+//  }
+
+
+function reverseList(head: ListNode | null): ListNode | null {
+
+    let prev = null;
+    let current = head;
+    let next = null;
+
+    while(current !== null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+};
+
