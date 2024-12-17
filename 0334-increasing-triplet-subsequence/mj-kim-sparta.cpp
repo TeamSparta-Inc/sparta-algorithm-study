73bb1dffae7d37e16a1392dc6a6d5bd705015717
mj-kim-sparta <mj.kim@teamsparta.co>
2024-12-17 13:51:59 +0900

    Time: 4 ms (28.19%), Space: 115.8 MB (14.25%) - LeetHub

--- 0334-increasing-triplet-subsequence/mj-kim-sparta.cpp
+++ 0334-increasing-triplet-subsequence/mj-kim-sparta.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        pair<int,int> p = {nums[0], INT_MAX};
-        for (int i = 1; i < nums.size(); i++) {
-            int num = nums[i];
-            if (num > p.second) return true;
-            else if (num > p.first) p.second = num;
-            else p.first = num;
+        int first = INT_MAX, second = INT_MAX;
+        for (int num: nums) {
+            if (num > second) return true;
+            else if (num > first) second = num;
+            else first = num;
         }
         return false;
     }
