dc673a161da8bcb467e7b26d3c56b520e5e1449f
ms-bang <ms.bang@teamsparta.co>
2025-05-13 13:02:09 +0900

    Time: 1 ms (77.04%), Space: 20 MB (38.63%) - LeetHub

--- 0009-palindrome-number/ms-bang.swift
+++ 0009-palindrome-number/ms-bang.swift
@@ -1,18 +1,23 @@
+import Foundation
+
 class Solution {
+    /// x가 팰린드롬이면 true, 아니면 false
+    @inline(__always)
     func isPalindrome(_ x: Int) -> Bool {
-        
+        // 1. 음수 or (끝이 0이면서 0이 아님) → 팰린드롬 불가
         if x < 0 || (x % 10 == 0 && x != 0) { return false }
         
-        var original = x         
-        var reversed = 0
+        var orig = x          // 앞쪽 절반
+        var rev  = 0          // 뒤쪽 절반(역순)
         
-        
-        while original > reversed {
-            let lastDigit = original % 10
-            reversed = reversed * 10 + lastDigit
-            original /= 10
+        // 2. 숫자의 절반만 뒤집기
+        while orig > rev {
+            rev  = rev * 10 + orig % 10
+            orig /= 10
         }
         
-        return original == reversed || original == reversed / 10
+        // 3. 짝수 자릿수: orig == rev
+        //    홀수 자릿수: orig == rev / 10 (가운데 숫자 제거)
+        return orig == rev || orig == rev / 10
     }
 }
