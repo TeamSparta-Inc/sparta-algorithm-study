0ba99fd0dafe650f1f60caf7e8d8de51404f9db2
js-yun1 <js.yun@teamsparta.co>
2025-08-19 12:49:12 +0900

    Time: 66 ms (15.83%), Space: 28.5 MB (13.59%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/js-yun1.py
+++ 2348-number-of-zero-filled-subarrays/js-yun1.py
@@ -1,7 +1,15 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
+        cache = {}
         def _calc_sub_subarray(n: int) -> List[int]:
             return [i for i in range(n, 0, -1)]
+            
+        def _count_sub_subarray(n: int) -> int:
+            if n in cache:
+                return cache[n]
+            
+            cache[n] = _calc_sub_subarray(n)
+            return cache[n]
             
         result = 0
         streak = 0
